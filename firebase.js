8ebb6a402b1f8439c217a397ff291de4875b60ef
chamodmadushan950-art <chamodmadushan950@gmail.com>
2026-08-03 20:20:45 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,50 +1,84 @@
 // Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
 import {
   getFirestore,
   collection,
   addDoc,
   getDocs,
-  updateDoc,
   deleteDoc,
-  doc
+  doc,
+  updateDoc
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
-// Replace these values with your Firebase project config
+
+// Your Firebase Configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.firebasestorage.app",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCq_QbfdHm1xwUKxNeWv02lydwMDOcSWJY",
+  authDomain: "lc-order-manager.firebaseapp.com",
+  projectId: "lc-order-manager",
+  storageBucket: "lc-order-manager.firebasestorage.app",
+  messagingSenderId: "412721669263",
+  appId: "1:412721669263:web:279d62967e184ed698bcfa",
+  measurementId: "G-700M9KDYND"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+
+// Firestore Database
 const db = getFirestore(app);
 
-// Save Order
-async function saveOrder(order) {
-  await addDoc(collection(db, "orders"), order);
-  alert("Order Saved!");
+
+// Add Order
+async function addOrderFirebase(order){
+
+    await addDoc(collection(db,"orders"), order);
+
+    alert("Order Saved Successfully");
 }
 
-// Load Orders
-async function loadOrders() {
-  const querySnapshot = await getDocs(collection(db, "orders"));
 
-  querySnapshot.forEach((docItem) => {
-    console.log(docItem.id, docItem.data());
-  });
+// Get Orders
+async function getOrders(){
+
+    let orders = [];
+
+    const querySnapshot = await getDocs(collection(db,"orders"));
+
+    querySnapshot.forEach((doc)=>{
+        orders.push({
+            id:doc.id,
+            ...doc.data()
+        });
+    });
+
+    return orders;
 }
+
 
 // Delete Order
-async function deleteOrder(id) {
-  await deleteDoc(doc(db, "orders", id));
+async function removeOrder(id){
+
+    await deleteDoc(doc(db,"orders",id));
+
 }
 
-// Export
-window.saveOrder = saveOrder;
-window.loadOrders = loadOrders;
-window.deleteOrder = deleteOrder;
+
+// Update Order
+async function editOrder(id,data){
+
+    await updateDoc(doc(db,"orders",id),data);
+
+}
+
+
+export {
+    db,
+    addOrderFirebase,
+    getOrders,
+    removeOrder,
+    editOrder
+};
